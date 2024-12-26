ef382578a72898c3eb459bf52aede96202289c46
temic137 <68796483+temic137@users.noreply.github.com>
2024-12-26 16:43:21 +0100

    5 commit

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,4 +1,4 @@
-import fitz
+import pymupdf
 import os
 import requests
 from bs4 import BeautifulSoup
@@ -12,13 +12,15 @@ logger = logging.getLogger(__name__)
 def extract_text_from_pdf(pdf_path):
     text_data = []
     try:
-        with fitz.open(pdf_path) as doc:
+        with pymupdf.open("a.pdf") as doc:
             for page_num, page in enumerate(doc, start=1):
-                text = page.get_text()
+                text = page.get_text().encode("utf8")
                 text_data.append({'page': page_num, 'text': text})
     except Exception as e:
         print(f"Error extracting text from PDF: {e}")
     return text_data
+
+
 
 def read_text_file(file_path):
     with open(file_path, 'r') as file:
