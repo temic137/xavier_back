83986c9adf80b3e5e17002d234892b2f54dafc2f
temic137 <68796483+temic137@users.noreply.github.com>
2025-01-05 00:38:15 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,23 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///crm.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'your_secret_key_here'
-    SESSION_TYPE = 'filesystem'
-    UPLOAD_FOLDER = 'uploads'
-
-
 # import os
 
 # class Config:
-#     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///crm.db')  # Use SQLite for local dev
+#     SQLALCHEMY_DATABASE_URI = 'sqlite:///crm.db'
 #     SQLALCHEMY_TRACK_MODIFICATIONS = False
 #     SECRET_KEY = 'your_secret_key_here'
 #     SESSION_TYPE = 'filesystem'
 #     UPLOAD_FOLDER = 'uploads'
+
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'your_secret_key_here'
+    SESSION_TYPE = 'filesystem'
+    UPLOAD_FOLDER = 'uploads'
