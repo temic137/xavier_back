80fb30b5b4c3150f3f6da512d9a0056fb493161c
temic137 <68796483+temic137@users.noreply.github.com>
2025-01-04 22:10:08 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
-# import os
-
-# class Config:
-#     SQLALCHEMY_DATABASE_URI = 'sqlite:///crm.db'
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-#     SECRET_KEY = 'your_secret_key_here'
-#     SESSION_TYPE = 'filesystem'
-#     UPLOAD_FOLDER = 'uploads'
-
-
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///crm.db')  # Use SQLite for local dev
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///crm.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'your_secret_key_here'
     SESSION_TYPE = 'filesystem'
     UPLOAD_FOLDER = 'uploads'
+
+
+# import os
+
+# class Config:
+#     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///crm.db')  # Use SQLite for local dev
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     SECRET_KEY = 'your_secret_key_here'
+#     SESSION_TYPE = 'filesystem'
+#     UPLOAD_FOLDER = 'uploads'
